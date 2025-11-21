efd0babaf5f135968182253b20909cad8fb278b6
soronmonika <otvosmonika25@gmail.com>
2025-11-21 13:03:08 +0100

    friisítve 20251121

--- 20251113/soron_monika_KCS2025/model/abKezelo.php
+++ 20251113/soron_monika_KCS2025/model/abKezelo.php
@@ -23,4 +23,85 @@ class AbKezelo{
     }
 
 
+        public static function getProducts():array{
+      $con=new mysqli("127.0.0.1", "root", "", "soron_monika_KCS2025");
+      $stmt=$con->prepare("SELECT * FROM product;");
+      $stmt->execute();
+      $stmt->bind_result($seid, $manufacturer, $model, $dataReceived);
+
+      $products=[];
+
+      while($stmt->fetch()){
+          $product=new Products($seid, $manufacturer, $model, $dataReceived);
+          $products=$product;
+      }
+
+      $stmt->close();
+      $con->close();
+
+      return $products;
+    }
+
+
+    public static function getService() : array{
+      $con=new mysqli("127.0.0.1", "root", "", "soron_monika_KCS2025");
+      $stmt=$con->prepare("SELECT * FROM service;");
+      $stmt->execute();
+      $stmt->bind_result($sid, $seid, $cid, $status, $lasUpdate);
+
+      $Service=[];
+
+      while ($stmt->fetch()){
+        $service=new Service($sid, $seid, $cid, $status, $lasUpdate);
+        $Service=$service;
+      }
+
+      $stmt->close();
+      $con->close();
+
+      return $Service;
+    }
+}
+
+//felvitel
+public static function createProduct(Product $product):void{
+    $con=new mysqli("127.0.0.1", "root", "", "soron_monika_KCS2025");
+    $stmt=$con->prepare("INSERT INTO Product VALUES (?,?,?,?);");
+
+    $seid=$Product->getSeid();
+    $manufacturer=$Product->getManufacturer();
+    $model=$Product->getModel();
+    $dataReceived=$product->getDataReceived();
+
+    $stmt->bind_param("ssss", $seid, $manufacturer, $model, $dataReceived);
+    $stmt->execute();
+
+    $con->close();
+}
+
+public static function createContactPerson(ContactPerson $contactPerson):void{
+  $con=new mysqli("127.0.0.1", "root", "", "soron_monika_KCS2025");
+  $stmt=$con->prepare("INSERT INTO ContactPerson VALUES (?,?,?,?);");
+
+  $cid=$ContactPerson->getCid();
+  $name=$ContactPerson->getName();
+  $phone=$ContactPerson->getPhone();
+  $email=$ContactPerson->getEmail();
+
+  $stmt->bind_param("ssis", $cid, $name, $phone, $email);
+  $stmt->execute();
+
+  $con->close();
+}
+
+public static function createService(Service $service):void{
+  $con=new mysqli("127.0.0.1", "root", "", "soron_monika_KCS2025");
+  $stmt=$con->prepare("INSER INTO  Service VALUS (?);");
+
+  $status=$Service->getStatus();
+
+  $stmt->bind_param("s", $status);
+  $stmt->execute();
+
+  $con->close();
 }
--- 20251113/soron_monika_KCS2025/model/service.php
+++ 20251113/soron_monika_KCS2025/model/service.php
@@ -1,17 +1,73 @@
 <?php
 
 class Service{
-  private string $sid;
-  private string $seid;
-  private string $cid;
-  private string $status;
-  private string $last_update;
+    private string $sid;
+    private string $seid;
+    private string $cid;
+    private string $status;
+    private string $lastUpdate;
 
-  public function __construct(string $sid, string $seid, string $cid, string $status, string $last_update){
-    $this->$sid;
-    $this->$seid;
-    $this->$cid;
-    $this->$status;
-    $this->$last_update;
+    public function __construct(string $sid, string $seid, string $cid, string $status, string $lastUpdate){
+      $this->setSid($sid);
+      $this->setSeid($seid);
+      $this->setCid($cid);
+      $this->setStatus($status);
+      $this->lastUpdate=$lastUpdate;
+    }
+
+  public function getSid():string{
+      return $this->sid;
+  }
+
+  public function getSeid():string{
+    return $this->seid;
+  }
+
+  public function getCid():string{
+    return $this-> cid;
+  }
+
+  public function getStatus() :string {
+    return $this->status;
+  }
+
+  public function getLastUpdate():string{
+    return $this->lastUpdate;
+  }
+
+  private function setSid(string $sid):void{
+    if(strlen(trim($sid))>0){
+      $this->sid=$sid;
+    }
+    else{
+      throw new InvalidArgumentException("A szervíz azonosító kötelező!");
+    }
+  }
+
+  private function setSeid(string $seid):void{
+    if(strlen(trim($seid))>0){
+    $this->seid=$seid;
+    }
+    else{
+      throw new InvalidArgumentException("A szériaszám kötelező!");
+    }
+  }
+
+  private function setCid(string $cid):void{
+    if(strlen(trim($cid))>0){
+      $this->cid=$cid;
+    }
+    else{
+      throw new InvalidArgumentException("A kapcsolattartóazonosító kötelező!");
+    }
+  }
+
+  private function setStatus(string $status):void{
+    if(strlen(trim($status))>0){
+      $this->status=$status;
+    }
+    else{
+      throw new InvalidArgumentException("Válassz státuszt!");
+    }
   }
 }
